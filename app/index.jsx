49a0edf719a75f7402fcbf6b35f643de3bb76bf4
Isaac J <jimenez.isaac.jr@gmail.com>
2024-06-12 21:09:15 -0700

    add tanstack query and modified environment

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,28 @@
 import { registerRootComponent } from 'expo';
 import { Text, View, ScrollView, TextInput } from 'react-native';
 import { NativeWindStyleSheet } from "nativewind";
+import  Banner  from '../components/Banner';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { QueryClient, QueryClientProvider, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
 
 NativeWindStyleSheet.setOutput({
-  default: "native",
+  default: "web",
 });
+const queryClient = new QueryClient();
 
 export default function App() {
+    
     return (
+     <QueryClientProvider client={queryClient}>
         <ScrollView>
            <View className="bg-green-200">
+            <Banner/>
             <Text>Test</Text>
            </View>
         </ScrollView >
+      </QueryClientProvider>
+      
     );
 }
 registerRootComponent(App);
---
+++ components/Banner.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function Banner() {
+  return (
+    <View className="h-20 mt-10 bg-slate-300">
+      <Text className="text-center text-xl
+      font-bold">Elden Ring Region Tracker</Text>
+    </View>
+  );
+};
+
