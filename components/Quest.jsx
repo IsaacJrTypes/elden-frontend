d4982e054a02dd7f918b071939380a3a8167488e
Isaac J <jimenez.isaac.jr@gmail.com>
2024-06-13 22:33:11 -0700

    add checkbox with data fetching. Need array state handling

--- components/Quest.jsx
+++ components/Quest.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { Text, View } from 'react-native';
 import {useRegions} from '../hooks/useRegions'
+import Checkbox from 'expo-checkbox';
 
 export default function Quest() {
-  
- const { data, isLoading, isError } = useRegions()
+    const { data, isLoading, isError } = useRegions()
 
-  if (isLoading) {
-    return <Text>Loading...</Text>;
-  }
-
-  if (isError) {
-    return <Text>Error fetching data</Text>;
-  }
-  if(data) {console.log(JSON.stringify(data,null,2))}
+    const [quests,setQuests] = useState([])
+    useEffect(() => {
+        if (data) {
+            setQuests(data)
+        }
+    }, [data])
+    if (isError) {
+        return <Text>Error fetching data</Text>;
+    }
 
   return (
     <View>
-      {data ? <Text>Data fetched: {JSON.stringify(data, null, 2)}</Text> : <Text>No data available</Text>}
+        <Text className="text-lg text-center">Quests</Text>
+        {quests.length !== 0 ? 
+            quests.map((entry) => {
+                return (
+                <View key={entry._id}>
+                    <Text>regionID:{entry.regionID}</Text>
+                    <Text>Region Name{entry.name}</Text>
+                    {entry.tasks.map((item)=> {
+                        return (
+                        <View key={item._id}>
+                            <Text>{item.description}</Text>
+                            <Checkbox value={item.complete}/>
+                        </View>)
+                    })}
+                </View>)
+            })
+            : 
+            <Text>Add a Region to create quest log</Text>
+        }
     </View>
   );
 }
